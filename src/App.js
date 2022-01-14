47c423c1bf1dd8c714330467635f8aa9b8303914
sadafamininia99 <aminicomputer96@gmail.com>
2022-01-14 15:00:35 +0330

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-x1">heloooooooooooooo</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>content</main>
+      </div>
+    </Router>
   );
 }
 
